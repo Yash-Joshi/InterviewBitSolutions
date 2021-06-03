81d9a6ba66ffb3dcdd1ddbd8388475c286e78293
Yash Joshi <iyashjoshi@gmail.com>
2021-06-03 21:03:32 +0530

    remove loop from linked list

---
+++ LinkedList/RemoveLoop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scaler.LinkedList
+{
+    public class RemoveLoop
+    {
+        public static ListNode solve(ListNode A)
+        {
+
+            // Create a Dictionary for visited node
+            // if seen again then that is the breaking condition 
+
+            ListNode temp = A;
+
+            Dictionary<ListNode, int> find = new Dictionary<ListNode, int>();
+
+            while (!find.ContainsKey(temp.next))
+            {
+                find.Add(temp, temp.val);
+                temp = temp.next;
+            }
+
+            temp.next = null;
+
+            return A;
+        }
+    }
+}
